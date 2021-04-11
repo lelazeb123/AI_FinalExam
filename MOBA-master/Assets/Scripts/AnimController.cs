aedb942cb5b20290e85e71d8e581dcc7a2c9ec44
Beej <61071579+lelazeb123@users.noreply.github.com>
2021-04-11 18:01:36 +0800

    Anim Fixed

---
+++ MOBA-master/Assets/Scripts/AnimController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimController : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    public Animator anim;
+
+    float motionSmoothTime = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = agent.velocity.magnitude / agent.speed;
+        anim.SetFloat("Blend", speed, motionSmoothTime, Time.deltaTime);
+    }
+}
